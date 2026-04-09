7b999ac6ec7b994953bf0151249090041d146e86
Priyanshu Karmakar <126000797+Priyanshu-Karmakar123@users.noreply.github.com>
2026-04-09 16:55:10 +0530

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Export environment variables with paths
+export OUTPUT_DIR="/scratch/sg/Priyanshu/TripCraft-main/zero_shot_cot_qwen_7day"
+export MODEL_NAME="qwen"     #qwen    #phi4             # Model name (you can change it as needed)
+export OPENAI_API_KEY="YOUR_OPENAI_KEY"             # Your OpenAI API key
+# export GOOGLE_API_KEY="YOUR_GOOGLE_KEY"                 # Your Google API key
+export DAY="7day"                                      # 3day/5day/7day
+export SET_TYPE="7day_gpt4o_orig"                            # Set type- name of folder in O/P directory where generated outputs get saved 
+export STRATEGY="direct_og"                                # direct_og / direct_param
+export CSV_FILE="/scratch/sg/Priyanshu/TripCraft-main/disruption_7day_annotation.csv"
+
+# Navigate to the planner directory
+cd tools/planner
+
+# Run the Python script with the environment variables
+python sole_planning_mltp.py \
+    --day $DAY \
+    --set_type $SET_TYPE \
+    --output_dir $OUTPUT_DIR \
+    --csv_file $CSV_FILE \
+    --model_name $MODEL_NAME \
+    --strategy $STRATEGY
+
